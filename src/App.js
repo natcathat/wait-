e83ae10fd35b8331d728882e49151435bdecfbdb
natcathat <54875683+natcathat@users.noreply.github.com>
2019-11-12 23:30:34 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return(
+ <Content />
+ );
+
+
+
 }
 
+function Content(){
+
+  return(
+  <div className="App">
+  <div id="diiv">
+    <p id="headline">Wait!</p>
+    <p id="text">Realize your real purpose of using social media app and ACHIEVE SOMETHING GREATER </p>
+    <p id="content">
+      <div>We all tend to lose track of our initial goal when we go into social media. Sometimes we blame our persistence to it, without knowing that all we need is actually a few seconds to contemplate our goals before using it. 
+</div>
+
+<div>You can think of this few seconds as the time to stop wrong intention and think about your real purpose; 
+or you can think of them as a chance for you to have a wider picture: do I want a morning of illusionary happiness or do I want a productive whole day? 
+</div>
+</p>
+<p id="new">
+<div>This is where our app Wait! will come in for you! </div>
+<div>It is designed to be bound with  social media apps like Wechat, Instagram, Facebook, etc. Every time you click into socil media app, a notification surface will pop up, asking you to wait a few seconds and think about  what  you want to do with the app this time. A quick single click only allows a single interface. If you want to switch surface,  just choose again. Daily, weekly, monthly summaries are available for comparisons of usage.
+</div>
+
+</p>
+</div>
+
+</div>
+
+
+  )
+
+}
+
+
 export default App;
+
